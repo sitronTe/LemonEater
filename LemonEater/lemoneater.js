2405a756d4a05d96e431c2287280f18bd0531c0f
Inge E. Halsaunet <halsaune@stud.ntnu.no>
2012-11-11 17:16:33 +0100

    A lemon eater now rolls across screen.
    A lot of changes made. Most important is we now have a board to
    play on. Lemons to eat. Points of interest.
    
    On the other hand, our LemonEater can not die. And it grows beyond
    all intended lengths. This however will be fixed in a while.

--- LemonEater/lemoneater.js
+++ LemonEater/lemoneater.js
@@ -1,32 +1,36 @@
 /*
- * Testing area
- */
-var koo = 1;
-function testWriter() {
-	// writing some to
-	var output = '<p class="lemon-background">';
-	output += '<span id="span1" class="lemon-eater-head">&nbsp;</span><span id="span2" class="lemon-eater-body">&nbsp;</span><br>';
-	output += '</p><p class="lemon-background">';
-	output += '<span id="span3" class="lemon-background">&nbsp;</span><span id="span4" class="lemon-eater-body">x</span>';
-	output += '</p>';
-	output += '<input type="button" value="alterclass" onclick="alterClass()" />';
-	document.getElementById("lemonEater").innerHTML = output;
-}
-function alterClass() {
-	if (koo == 1) {
-		document.getElementById("span3").className = "lemon-eater-body";
-		koo++;
-	} else {
-		document.getElementById("span3").className = "";
-		koo = 1;
-	}
-}
-/*
- * Objects area - lemonEater -> an eater of lemons.
+ * Objects area -
+ * 	LemonPoint -> a 2 dimensional point.
+ * 	LemonLemon -> a lemon to eat! Yummi!
+ * 	LemonEater -> an eater of lemons.
+ * 	LemonBoard -> a place to move around. Loaded with lemons!
  */
 
 /**
- * Creates a new lemonEater. Placed at (placeX,placeY). Length minimum set to 1.
+ * Creates a new LemonPoint. Sets x and y values according to arguments.
+ * 
+ * @param placeX
+ *            x-position of point
+ * @param placeY
+ *            y-position of point
+ */
+function LemonPoint(placeX, placeY) {
+	this.x = placeX * 1;
+	this.y = placeY * 1;
+}
+
+/**
+ * Creates a delicious lemon! Placed at the place brought along. Yummi!
+ * 
+ * @param place
+ *            position of the lemon
+ */
+function LemonLemon(place) {
+	this.place = new LemonPoint(place.x, place.y);
+}
+
+/**
+ * Creates a new LemonEater. Placed at lemonPoint. Length minimum set to 1.
  * Direction is either (n)orth, (s)outh, (e)ast or (w)est. If none of these,
  * direction is set to (n)orth.
  * 
@@ -38,20 +42,71 @@ function alterClass() {
  *            length of lemonEater. Minimum set to 4
  * @param direction
  *            direction to start moving in
+ * @param context
+ *            LemonBoard this LemonEater lives in.
  * @returns null
  */
-function LemonEater(placeX, placeY, length, direction) {
+function LemonEater(place, length, direction, context) {
+	// TODO head is never repainted. Drawn length just increases. Death seems impossible.
+	this.ownerBoard = context;
+	this.lemonsEaten = 0;
+	this.previousLength = 1;
+	this.x = place.x;
+	this.y = place.y;
 	this.dx = 0;
 	this.dy = 0;
 	this.setDirection = setEaterDirection;
 	this.tick = eaterTick;
 	this.setDirection(direction);
-	this.x = placeX * 1;
-	this.y = placeY * 1;
+	this.contains = eaterContains;
+
+	this.positions = new Array();
+	this.positions[0] = new LemonPoint(place.x, place.y);
 	if (length > 0)
 		this.length = length;
 	else
 		this.length = 1;
+	// Animation functions
+	this.drawHead = eaterDrawHead;
+	this.drawBody = eaterDrawBody;
+	this.drawHead(this.positions[0]);
+}
+
+/**
+ * Checks if a point is in this eater.
+ * 
+ * @param point
+ *            the place to check
+ * @returns {Boolean} if if contained the point.
+ */
+function eaterContains(point) {
+	for ( var i = 0; i < this.positions.length; i++) {
+		if ((this.positions[i].x == point.x)
+				&& (this.positions[i].y == point.y)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * Draws eater head to this position
+ * 
+ * @param point
+ *            where the head is drawn
+ */
+function eaterDrawHead(point) {
+	document.getElementById('lemon-cell-x' + point.x + '-y' + point.y).className = 'lemon-eater-head';
+}
+
+/**
+ * Draws eater body to this position
+ * 
+ * @param point
+ *            where the body is drawn
+ */
+function eaterDrawBody(point) {
+	document.getElementById('lemon-cell-x' + point.x + '-y' + point.y).className = 'lemon-eater-body';
 }
 
 /**
@@ -80,6 +135,7 @@ function setEaterDirection(direction) {
 		this.dx = 0;
 	}
 }
+
 /**
  * A single tick of the LemonEater
  */
@@ -91,11 +147,156 @@ function eaterTick() {
 		this.x = 0;
 	this.y += this.dy;
 	if (this.y < 0)
-		this.y = heigth - 1;
+		this.y = height - 1;
 	if (this.y >= height)
 		this.y = 0;
-	// TODO perform some checks if we are alive.
-	// TODO Make something for animation.
+	var point = new LemonPoint(this.x, this.y);
+	// Survive check
+	var pointNotOk = this.contains(point) || this.ownerBoard.isWall(point);
+	if (pointNotOk) {
+		// TODO We have died. Do something!
+	} else {
+		// consume lemon check
+		if (this.ownerBoard.consumeLemon(point)) {
+			var l = this.previousLength + this.length;
+			this.previousLength = this.length;
+			this.length = l;
+		}
+		// Animation tasks
+		this.drawHead(point);
+		this.drawBody(this.positions[0]);
+		// if we are too long, remove last item and release it.
+		if (this.positions.length > this.length) {
+			var rel = this.positions.pop();
+			this.ownerBoard.release(rel);
+		}
+		// add to our positions. This is done after item removal to ensure that
+		// length is body length without head.
+		for ( var i = this.positions.length; i > 0; i--) {
+			this.positions[i] = this.positiond[i - 1];
+		}
+		this.positions[0] = point;
+	}
+}
+
+/**
+ * Creates a new LemonBoard. When completed, start position and walls should be
+ * presented to this as a String. Right now this variable does nothing.
+ * 
+ * @param boardAsString
+ *            a String representation of the board
+ */
+function LemonBoard(boardAsString) {
+	this.lemon = new LemonLemon(new LemonPoint(-1,-1));
+	this.walls = new Array();
+	for ( var y = 0; y < height; y++) {
+		this.walls[y] = new Array();
+		for ( var x = 0; x < width; x++) {
+			// No walls at this moment.
+			this.walls[y][x] = 0;
+		}
+	}
+	var start = new LemonPoint(5, 5);
+	this.eater = new LemonEater(start, 1, 'e', this);
+	this.isWall = boardWall;
+	this.isLemon = boardIsLemon;
+	this.tick = boardTick;
+	this.spawnLemon = boardSpawnLemon;
+	this.release = boardRelease;
+	this.spawnLemon();
+	this.consumeLemon = boardConsume;
+	this.setEaterDirection = boardEaterDirection;
+}
+
+/**
+ * Sets this boards eaters direction.
+ */
+function boardEaterDirection(direction) {
+	this.eater.setDirection(direction);
+}
+
+/**
+ * Checks if the point of interest is a wall.
+ * 
+ * @param point
+ *            point to check
+ * @returns {Boolean} if this is a wall segment.
+ */
+function boardWall(point) {
+	var isWall = this.walls[point.y][point.x] > 0;
+	return isWall;
+}
+
+/**
+ * Tries to consume lemon at this point. If no lemon present, return false.
+ * 
+ * @param point
+ *            point to check
+ * @returns {Boolean} if a lemon was eaten.
+ */
+function boardConsume(point) {
+	var isLemon = this.isLemon(point);
+	if (isLemon) {
+		this.lemon = this.spawnLemon();
+	}
+	return isLemon;
+}
+
+/**
+ * Checks if there is a lemon on this point
+ * 
+ * @param point
+ *            point to check
+ * @returns {Boolean} if there is a lemon here.
+ */
+function boardIsLemon(point) {
+	var isLemon = (this.lemon.place.x == point.x)
+			&& (this.lemon.place.y == point.y);
+	return isLemon;
+}
+
+/**
+ * Spawns a new lemon!
+ * 
+ * @returns {LemonLemon} the lemon spawned.
+ */
+function boardSpawnLemon() {
+	var x = Math.floor(Math.random() * width);
+	var y = Math.floor(Math.random() * height);
+	var point = new LemonPoint(x, y);
+	var illegal = this.isWall(point) || this.eater.contains(point);
+	while (illegal) {
+		x = Math.floor(Math.random() * width);
+		y = Math.floor(Math.random() * height);
+		point = new LemonPoint(x, y);
+		illegal = this.isWall(point) || this.eater.contains(point);
+	}
+	this.lemon = new LemonLemon(point);
+	this.release(point);
+	return this.lemon;
+}
+
+/**
+ * Removes anything that was drawn on top of this point.
+ * 
+ * @param point
+ *            The point to release.
+ */
+function boardRelease(point) {
+	var classType = "";
+	if (this.isWall(point)) {
+		classType = "lemon-wall-" + this.walls[point.y][point.x];
+	} else if (this.isLemon(point)) {
+		classType = "lemon-lemon";
+	}
+	document.getElementById('lemon-cell-x' + point.x + '-y' + point.y).className = classType;
+}
+
+/**
+ * A single tick of this board.
+ */
+function boardTick() {
+	this.eater.tick();
 }
 
 /*
@@ -111,19 +312,45 @@ var refreshRate = 100;
 var started = false;
 var testCellX = width - 1;
 var testCellY = height - 1;
+var gameBoard;
 
 /**
  * Initialize the game
  */
 function lemonInit() {
 	lemonDrawBoard();
+	// TODO this is testing things:
+	gameBoard=new LemonBoard("");
+	started = true;
+	// TODO End testing things
 	// Add listeners and focus.
 	var gameArea = document.getElementById('lemon-focus-controller');
 	gameArea.onkeydown = function(e) {
-		//alert(e.keyCode);
+		switch (e.keyCode) {
+		case 65:
+		case 37:
+			// left
+			gameBoard.setEaterDirection('w');
+			break;
+		case 87:
+		case 38:
+			// up
+			gameBoard.setEaterDirection('n');
+			break;
+		case 68:
+		case 39:
+			// right
+			gameBoard.setEaterDirection('e');
+			break;
+		case 83:
+		case 40:
+			// down
+			gameBoard.setEaterDirection('s');
+			break;
+		}
 	};
 	gameArea.onkeyup = function(e) {
-		// something e.keyCode
+		// unused i think
 	};
 	gameArea.focus();
 	setInterval("lemonTick()", refreshRate);
@@ -166,7 +393,7 @@ function lemonTick() {
 	if (started) {
 		lemonGameTick();
 	}
-
+/*
 	// Testing animation, since game cannot start at this moment, no problem.
 	document.getElementById('lemon-cell-x' + testCellX + '-y' + testCellY).innerHTML = empty;
 	document.getElementById('lemon-cell-x' + testCellX + '-y' + testCellY).className = "lemon-eater-body";
@@ -176,11 +403,14 @@ function lemonTick() {
 	testCellY--;
 	if (testCellY < 0)
 		testCellY = height - 1;
+*/
 }
 
 /**
  * If game is started, this should happen
  */
 function lemonGameTick() {
-	//
+	// TODO
+	// board does not tick...
+	gameBoard.tick();
 }
